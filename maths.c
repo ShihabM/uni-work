62fcf7f1823e9421afad333c7f1b6acc885a802e
Shihab Mehboob <P13233877@GH584-005.lec-admin.dmu.ac.uk>
2015-01-12 14:05:22 +0000

    Added maths functions

---
+++ maths.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	int x = 7;
+	int y = 3;
+	printf ("%i + %i = %i\n", x, y, x+y);
+	printf ("%i - %i = %i\n", x, y, x-y);
+	printf ("%i * %i = %i\n", x, y, x*y);
+	printf ("%i / %i = %i\n", x, y, x/y);
+	return 0;
+
+}
